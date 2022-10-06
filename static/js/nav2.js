a5c74526f407e063e127db062ca27eb83710cc12
sudomaster00081 <a.jayprasadd0008@gmail.com>
2022-10-06 07:34:22 +0530

    +Added Basic UI ,Functionality left 😊

---
+++ static/js/nav2.js
@@ -0,0 +1,13 @@
+document.write('<div class="row">');
+document.write('            <div class=" col-lg-10  col-md-9 col-6"><img src="/images/logo.png" width="100" height="100" alt="logo"></div>');
+document.write('            <div class="  col-lg-2 col-md-3  col-6 mt-3">');
+document.write('                <ul class="list-unstyled list-inline">');
+document.write('                    <li class="list-inline-item"><i class="fas fa-user" style="color:#1F2F98;"></i></li>');
+document.write('                    <li class="list-inline-item">');
+document.write('                        <h5 class="texte">Log out</h5>');
+document.write('                    </li>');
+document.write('                    <li class="list-inline-item"> <i class="fas fa-bell" style="color:#1F2F98;"></i></li>');
+document.write('                    <li class="list-inline-item"><i class="fas fa-moon" style="color:#1F2F98;"></i></li>');
+document.write('                </ul>');
+document.write('            </div>');
+document.write('        </div>');
